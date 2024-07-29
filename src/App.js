2e33b628a1effe3b47a81436593931c0f50de6b1
bmbx08 <bmbx08@bu.ac.kr>
2024-07-30 03:51:40 +0900

    added normal fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './page/HomePage';
+import ReactQueryPage from './page/ReactQueryPage';
+import NormalPage from './page/NormalPage';
 
 function App() {
   return (
     <div>
-
+      <nav style={{backgroundColor: "beige", padding: "20px"}}>
+        <Link to="/" style={{marginRight:"10px"}}>
+          Homepage
+        </Link>
+        <Link to="/normal-page" style={{marginRight:"10px"}}>Normal fetch</Link>
+        <Link to="/react-query">React Query</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/react-query" element={<ReactQueryPage />}/>
+        <Route path="/normal-page" element={<NormalPage/>}/>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right"/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
   </QueryClientProvider>
 );
 
